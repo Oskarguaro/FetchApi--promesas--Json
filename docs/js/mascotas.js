7b16e68715cccc7562ac2c01ce5dc3871f3e24ef
OskarG <oscarguarin@hotmail.es>
2022-01-19 12:51:57 -0500

    Agregué anclas HTML y redireccioné la API Json

--- docs/js/mascotas.js
+++ docs/js/mascotas.js
@@ -15,7 +15,7 @@ const fetchData = async () => {
     // console.log("hola");
     try {
         
-        const res = await fetch("https://raw.githubusercontent.com/Oskarguaro/FetchApi--promesas--Json/main/data.json");
+        const res = await fetch("https://raw.githubusercontent.com/Oskarguaro/FetchApi--promesas--Json/main/docs/data.json");
         // const res = await fetch("https://gist.githubusercontent.com/davidcp22/d1967026b327ce66af7371725cea4c8a/raw/6fc6ca5f1eb5d9f0118b948362bc1d844f424c6b/animales.json");
         const data = await res.json();
         console.log(data);
